62c6655f7da66b0f89a48554a8eba7e697ea36eb
Daniele Silvestro <silvestro.daniele@gmail.com>
2024-10-11 08:46:20 +0200

    Update hard_voting.R

--- R/hard_voting.R
+++ R/hard_voting.R
@@ -9,26 +9,25 @@
 #' @return A dataframe
 #' @export
 hard_voting <- function(listOfdataframe){
-  HVdata <- datasets[[1]]
+  HVdata <- listOfdataframe[[1]]
+  
+  for (d in 1:ncol(HVdata)) {
 
-  for(d in 1:ncol(HVdata)){
-
-    columnstoHV <- data.frame(matrix(NA, nrow = nrow(HVdata), ncol = length(datasets)))
-    picked <- sapply(datasets,`[`, d)
-
-    for(col in 1:length(picked)){
-      columnstoHV[,col] <- picked[[col]]
+    columnstoHV <- data.frame(matrix(NA, nrow = nrow(HVdata), 
+                                     ncol = length(listOfdataframe)))
+    picked <- sapply(listOfdataframe, `[`, d)
+    for (col in 1:length(picked)) {
+      columnstoHV[, col] <- picked[[col]]
     }
-
     #categorical variable
-    if(is.factor(picked[[1]])){
-      HVdata[,d] <- as.factor(apply(columnstoHV, 1, function(x) names(which.max(table(x)))))
+    if (is.factor(picked[[1]])) {
+      HVdata[, d] <- as.factor(apply(columnstoHV, 1, function(x) names(which.max(table(x)))))
     }
     #continuous variable
-    else{
-      HVdata[,d] <- as.numeric(apply(columnstoHV, 1, mean))
+    else {
+      HVdata[, d] <- as.numeric(apply(columnstoHV, 1, 
+                                      mean))
     }
   }
-
   return(HVdata)
 }
